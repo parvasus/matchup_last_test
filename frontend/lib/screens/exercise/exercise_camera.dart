029747a229a7f7a43f4f843cfe4ebec466c60b55
parvasus <2021142023@tukorea.ac.kr>
2024-10-27 22:34:49 +0900

    Git->Commit 실행 1회차
    1. url 주소 바꿈
    2. exercise_camera.dart의 웹소켓 서버 주소를 localhost로 할지, url 주소로 할지 고민중

--- frontend/lib/screens/exercise/exercise_camera.dart
+++ frontend/lib/screens/exercise/exercise_camera.dart
@@ -20,11 +20,12 @@ class ExerciseCameraScreen extends StatefulWidget {
   @override
   _ExerciseCameraScreenState createState() => _ExerciseCameraScreenState();
 }
-
+// 운동을 할 때, 실시간으로 자세를 분석하고 피드백을 제공하는 역할 : _ExerciseCameraScreenState
+// 웹소켓은 열어두고 구동하는 기능임
 class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
   late Timer _timer;
   late Timer _coordinateTimer;
-  int _remainingTime = 10;  // 10초 타이머 설정
+  int _remainingTime = 10;  // 10초 후에 운동 시작되도록 타이머 설정
   bool _isExercising = false;
   late WebSocketChannel _channel;
   List<Pose> _detectedPoses = [];
@@ -38,6 +39,7 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
   String lastSpokenFeedback = "";
   bool isSpeaking = false;
 
+  // TTS, 웹소켓, 타이머 : initState() 함수로 실행
   @override
   void initState() {
     super.initState();
@@ -48,6 +50,7 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
     _startTimer();
   }
 
+  // 실시간 피드백을 받아 음성 피드백(TTS) 제공
   void _initializeTTS() {
     flutterTts.setLanguage('ko-KR').catchError((error) {
       print("Error setting language: $error");
@@ -63,10 +66,13 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
       });
     });
   }
-
+  // 지정된 웹소켓 주소로 연결
   void _connectWebSocket() {
     try {
       _channel = WebSocketChannel.connect(
+        // 원격 서버 주소가 13.124.114.252로 설정되어 있는데,
+        // localhost로 하던가 아니면 내 PC의 주소로 바꾸자 :192.168.35.91
+        // Uri.parse('ws://localhost:8000/api/v1/exercise/ws'),
         Uri.parse('ws://13.124.114.252:8000/api/v1/exercise/ws'),
         // Uri.parse('ws://10.254.3.138:8000/api/v1/exercise/ws'),
       );
@@ -74,7 +80,8 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
       final userProvider = Provider.of<UserProvider>(context, listen: false);
       String? accessToken = userProvider.accessToken;
       _channel.sink.add(jsonEncode({
-        "exercise_id": widget.exerciseId,
+        // 연결된 세션을 식별하고 사용자를 인증하기 위해 초기 데이터 보냄(운동 종류
+        "exercise_id": widget.exerciseId, // 운동 종류(목, 스쿼트, 다리, 허리)
         "access_token": accessToken,
       }));
 
@@ -107,6 +114,7 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
                 realCount = data['counter'] ?? 0;
                 sets = data['sets'] ?? 0;
                 print('Updated realCount: $realCount, sets: $sets');
+                // lastSpokenFeedback : 음성 피드백 중복 피하도록. 새로운 피드백만 말함
                 if (feedback.isNotEmpty && feedback != lastSpokenFeedback && !ttsQueue.contains(feedback)) {
                   ttsQueue.add(feedback);
                   if (!isSpeaking) {
@@ -168,12 +176,13 @@ class _ExerciseCameraScreenState extends State<ExerciseCameraScreen> {
       await flutterTts.speak(message);
     }
   }
-
+  
+  // 위젯 삭제 시 호출 : 웹소켓 닫고 타이머 중지 = 리소스 확보용
   @override
   void dispose() {
     _timer.cancel();
     if (_coordinateTimer.isActive) {
-      _coordinateTimer.cancel();
+      _coordinateTimer.cancel(); // 웹소켓 닫기
     }
     _channel.sink.close();
     super.dispose();
