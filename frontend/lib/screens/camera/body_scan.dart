d6839ec5f282ad3c77551d02bf4996e96126e2ff
parvasus <2021142023@tukorea.ac.kr>
2024-11-02 19:18:23 +0900

    정면측면 -> 정면 + 각종 주석

--- frontend/lib/screens/camera/body_scan.dart
+++ frontend/lib/screens/camera/body_scan.dart
@@ -24,9 +24,9 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
   late Future<void> _initializeControllerFuture;
   late CameraController _controller;
   late List<CameraDescription> _cameras;
-  int _photoCount = 0;
-  int _remainingTime = 8;
-  late Timer _timer;
+  int _photoCount = 0; // 현재 촬영된 사진 수
+  int _remainingTime = 5; // 타이머 카운트다운 시간
+  late Timer _timer; // 타이머 객체
 
   @override
   void initState() {
@@ -34,6 +34,7 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
     _initializeControllerFuture = initializeCamera();
   }
 
+  // 카메라 초기화 함수
   Future<void> initializeCamera() async {
     _cameras = await availableCameras();
     _controller = CameraController(
@@ -49,6 +50,7 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
     }
   }
 
+  // 사진을 캡처하고 저장하는 함수
   Future<void> takePicture() async {
     if (!_controller.value.isInitialized) {
       return;
@@ -59,18 +61,19 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
       final Directory directory = await getApplicationDocumentsDirectory();
       final String imagePath =
           join(directory.path, 'Photo_${DateTime.now()}.jpg');
-      await File(file.path).copy(imagePath);
+      await File(file.path).copy(imagePath);    // 캡처된 사진을 지정된 경로에 저장
       setState(() {
-        _photoCount++;
+        _photoCount++;        // 촬영된 사진 수 업데이트
       });
-      print('Picture saved to $imagePath');
+      print('$imagePath : Capture Success!!!'); // 캡처 성공 시 메시지 출력
 
-      await _uploadImage(File(imagePath));
+      await _uploadImage(File(imagePath));      // 저장한 이미지를 서버로 업로드
     } catch (e) {
-      print('Error taking picture: $e');
+      print('Error taking picture: $e');        // 오류 발생 시 에러 메시지 출력
     }
   }
 
+  // 서버에 이미지를 업로드하는 함수
   Future<void> _uploadImage(File imageFile) async {
     final uri = Uri.parse("$baseUrl/health/upload/");
     var request = http.MultipartRequest('POST', uri);
@@ -78,6 +81,7 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
       var pic = await http.MultipartFile.fromPath(
         "file",
         imageFile.path,
+        // jpeg 저장
         contentType: MediaType('image', 'jpeg'),
       );
       request.files.add(pic);
@@ -106,19 +110,17 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
     }
   }
 
+  // 카운트다운을 시작하는 함수 (3초 남았을 때 캡처 실행)
   void startCountdown() {
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       if (mounted) {
         setState(() {
           if (_remainingTime > 0) {
             _remainingTime--;
-          } else {
+          } else { // 0초에 시간 끄고, 사진 찍고
             _timer.cancel();
             takePicture();
-            _remainingTime = 8;
-            if (_photoCount < 1) {
-              startCountdown();
-            }
+            // 2번 반복하는거 취소함
           }
         });
       } else {
@@ -129,8 +131,8 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
 
   @override
   void dispose() {
-    _controller.dispose();
-    _timer.cancel();
+    _controller.dispose();  // 카메라 컨트롤러 해제
+    _timer.cancel();        // 타이머 취소
     super.dispose();
   }
 
@@ -144,20 +146,21 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
             if (!_controller.value.isInitialized) {
               return Center(child: Text('Error: Failed to initialize camera.'));
             }
-            return buildCameraPreview();
+            return buildCameraPreview();      // 카메라 미리보기 화면
           } else {
-            return Center(child: CircularProgressIndicator());
+            return Center(child: CircularProgressIndicator()); // 로딩 스피너
           }
         },
       ),
     );
   }
 
+  // 카메라 미리보기(buildCameraPreview)를 렌더링하는 위젯
   Widget buildCameraPreview() {
     return Stack(
       children: [
         Positioned.fill(
-          child: CameraPreview(_controller),
+          child: CameraPreview(_controller),// 카메라 미리보기 출력
         ),
         Align(
           alignment: Alignment.bottomCenter,
@@ -176,7 +179,7 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
                   children: [
                     SizedBox(height: 10),
                     ElevatedButton(
-                      onPressed: _photoCount < 2 ? startCountdown : null,
+                      onPressed: _photoCount < 1 ? startCountdown : null, // 1장만 촬영 가능
                       style: ElevatedButton.styleFrom(
                         shape: CircleBorder(),
                         padding: EdgeInsets.all(20),
@@ -191,14 +194,14 @@ class _BodyScanScreenState extends State<BodyScanScreen> {
                     SizedBox(height: 10),
                     Text.rich(
                       TextSpan(
-                        text: '$_remainingTime 초 후에 ',
+                        text: '$_remainingTime 초 후에 ',    // 남은 시간 표시
                         style: TextStyle(
                             color: Colors.black,
                             fontSize: 30,
                             fontWeight: FontWeight.bold),
                         children: <TextSpan>[
                           TextSpan(
-                            text: _photoCount == 0 ? '정면 촬영' : '측면 촬영',
+                            text: '정면 촬영', // 촬영 대상 (정면)
                             style: TextStyle(
                                 color: Colors.black,
                                 fontWeight: FontWeight.bold),
