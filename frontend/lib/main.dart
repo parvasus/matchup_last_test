beafc53684aaa72858577fee9db1ef5513067658
parvasus <2021142023@tukorea.ac.kr>
2024-11-02 10:59:14 +0900

    그냥 backend에 install 함 1. pip install fastapi-another-jwt-auth 2. pip install PyJWT

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         builder: (context, userProvider, child) {
           return userProvider.isLoggedIn
               ? BottomNavBar(accessToken: userProvider.accessToken ?? '')
-              : ExerciseScreen();
+              : LoginScreen();
         },
       ),
     );
